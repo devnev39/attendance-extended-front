73b7d2243cc727dbfc92ce56d87f688b70a62863
Dhruv Bonde <devnev39@gmail.com>
2023-12-30 13:43:29 +0530

    url change ngrok

--- src/config/url.js
+++ src/config/url.js
@@ -1,4 +1,5 @@
 export const Urls = {
     _baseUrl: "http://localhost:8080",
-    baseUrl: "https://attendance-extended-back.onrender.com",
+    __baseUrl: "https://attendance-extended-back.onrender.com",
+    baseUrl: "https://68e5-210-16-95-148.ngrok-free.app"
 }
