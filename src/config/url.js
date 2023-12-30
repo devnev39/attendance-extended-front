dd525dd858eb7cd07146a00d151306b750374ef2
Dhruv Bonde <devnev39@gmail.com>
2023-12-30 13:46:29 +0530

    url change render

--- src/config/url.js
+++ src/config/url.js
@@ -1,5 +1,5 @@
 export const Urls = {
     _baseUrl: "http://localhost:8080",
-    __baseUrl: "https://attendance-extended-back.onrender.com",
-    baseUrl: "https://68e5-210-16-95-148.ngrok-free.app"
+    baseUrl: "https://attendance-extended-back.onrender.com",
+    __baseUrl: "https://68e5-210-16-95-148.ngrok-free.app"
 }
